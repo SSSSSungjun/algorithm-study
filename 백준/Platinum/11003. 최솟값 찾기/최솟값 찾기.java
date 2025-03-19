8c8db51b3dcd70e95bb70f9c7e850b035f77ec7c
Sungjun Yun <108875242+SSSSSungjun@users.noreply.github.com>
2025-03-19 19:07:49 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2304 ms, Memory: 962644 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -7,33 +7,28 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // Читаем N
-        int L = Integer.parseInt(st.nextToken()); // Читаем L
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
 
-        Deque<int[]> deque = new ArrayDeque<>(); // Дек для хранения минимального значения в окне
+        Deque<int[]> deque = new ArrayDeque<>();
 
-        st = new StringTokenizer(br.readLine()); // Читаем массив чисел
+        st = new StringTokenizer(br.readLine()); 
         for (int i = 0; i < N; i++) {
             int input = Integer.parseInt(st.nextToken());
-
-            // Удаляем из дека все элементы, которые больше текущего числа
+            
             while (!deque.isEmpty() && deque.peekLast()[0] > input) {
                 deque.pollLast();
             }
-
-            // Добавляем текущий элемент и его индекс в дек
+            
             deque.offerLast(new int[]{input, i});
-
-            // Если первый элемент дека выходит за границы окна, удаляем его
+            
             if (deque.peekFirst()[1] <= i - L) {
                 deque.pollFirst();
             }
-
-            // Добавляем минимальный элемент текущего окна в результат
             sb.append(deque.peekFirst()[0]).append(" ");
         }
         System.out.println(sb);
     }
 }
-// Если Scanner не работает, а BufferedReader работает, это будет немного раздражать
-// Но все равно попробуем!
+// Scanner로는 안풀리고 BufferedReader로는 풀리면 좀 화날거같다
+// 일단 해본다
