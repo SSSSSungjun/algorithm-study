6ea53e07b40dba32e6892d5bf3f5ea41f6be1dd8
Sungjun Yun <108875242+SSSSSungjun@users.noreply.github.com>
2025-06-18 00:24:35 +0900

    [Gold IV] Title: 거짓말, Time: 204 ms, Memory: 17976 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,79 +1,60 @@
-
-import java.io.*;
+import java.io.IOException;
 import java.util.*;
 
-public class Main {
+class Main {
 
-    static int N,M;
-    static int[] parents;
+    static int n;
+    static int m;
+    static int tn;
+    static ArrayList<Integer>[] party;
+    static ArrayList<Integer> al;
+    static boolean[] visited;
 
-    static int find(int n) {
-        if(parents[n]==n)return n;
+    public static void main(String[] args) throws IOException {
+        
+        Scanner sc = new Scanner(System.in);
 
-        return parents[n]=find(parents[n]);
-    }
+        n = sc.nextInt();
+        m = sc.nextInt();
+        party = new ArrayList[m];
+        visited = new boolean[m];
 
-    static void Union(int a, int b) {
-        int findA=find(a);
-        int findB=find(b);
+        for(int i=0; i<m; i++) party[i] = new ArrayList<>();
 
-        if(findA>findB) {
-            parents[findA]=findB;
-        }else {
-            parents[findB]=findA;
-        }
+        tn = sc.nextInt();
+        al = new ArrayList<>();
+        for(int j=0; j<tn; j++) al.add(sc.nextInt());
 
-    }
+        for(int i=0; i<m; i++) {
+            int pn = sc.nextInt();
 
-    static boolean isUnion(int a, int b) {
-        return parents[a]==parents[b];
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-
-        String[] nm=br.readLine().split(" ");
-        N=Integer.parseInt(nm[0]); // 사람 수
-        M=Integer.parseInt(nm[1]); // 파티 수
-        parents=new int[N+1];
-
-        for(int i=0;i<N+1;i++)parents[i]=i;
-
-        int[] known= Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        //거짓말 데이터.
-
-        for(int i=2;i<known.length;i++){
-            Union(known[i-1],known[i]);
-        } //진실을 아는 자
-
-        int[][] party=new int[M][];
-
-        for(int i=0;i<M;i++){
-            party[i]=Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-            if(party.length!=2){
-                for(int k=2;k<party[i].length;k++){
-                    Union(party[i][k-1],party[i][k]);
-                }
-            }
-
-        } //파티참여자 전부다 유니온
-
-        for(int i=1;i<N+1;i++)find(i); //파인드갱신
-
-
-        int result=0;
-        if(known.length==1)result=M;
-        else{
-            for(int i=0;i<M;i++){
-                if(!isUnion(known[1],party[i][1])) result++;
+            for(int j=0; j<pn; j++) {
+                party[i].add(sc.nextInt());
             }
         }
-        //System.out.println(Arrays.toString(parents));
-        System.out.println(result);
+
+        recursive();
+
+        int count = 0;
+        for(boolean b : visited) {
+            if(!b) count++;
+        }
+
+        System.out.println(count);
 
     }
 
+    private static void recursive() {
+        for(int i=0; i<m; i++) {
+            if(visited[i]) continue;
+            for(int j=0; j<party[i].size(); j++) {
+                if(al.contains(party[i].get(j))) {
+                    al.addAll(party[i]);
+                    visited[i] = true;
+                    recursive();
+                    return;
+                }    
+            }
+        }
+    }
 }
-
